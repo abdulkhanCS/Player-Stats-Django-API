c4629ffb35a50be958d51704f7bb83d10b535ad7
Abdul Khan <absulkhancs@gmail.com>
2021-02-01 14:05:47 -0500

    heroku deployment preparation

--- src/api/settings.py
+++ src/api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
