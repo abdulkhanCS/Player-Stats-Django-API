a98517b61ca2c928ae060e81a28a13a8fb16d4d0
Abdul Khan <absulkhancs@gmail.com>
2021-02-07 11:16:50 -0500

    fix

--- Procfile
+++ Procfile
@@ -5,4 +5,4 @@ web: waitress-serve \
     --log-untrusted-proxy-headers \
     --clear-untrusted-proxy-headers \
     --threads ${WEB_CONCURRENCY:-4} \
-    wsgi:application
+    settings:WSGI_APPLICATION
--- src/api/settings.py
+++ src/api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ce-ntr^1vf3b85@xt4n01m)!*j6nd0pa@o#z*zbo)8=(kwn2@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
