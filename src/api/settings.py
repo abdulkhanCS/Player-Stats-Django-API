f88b1c378eb092a83a715389708fe0c51a28756d
Abdul Khan <abdulkhan@Abduls-MacBook-Air.local>
2021-04-20 13:48:17 -0400

    heroku

--- src/api/settings.py
+++ src/api/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'src/playerIDs.txt')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 #django_heroku.settings(locals())
 #serve(WSGI_APPLICATION)
